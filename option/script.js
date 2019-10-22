bb8b1adfb79bceda8e26fdd1328aad844fb3f157
Uut <andreasluhut2911@gmail.com>
2019-10-23 05:21:19 +0700

    Option stroage

--- option/script.js
+++ option/script.js
@@ -14,27 +14,53 @@
         }
     };
 
+    const storage = {
+        interface: {
+            screenOnly: false
+        },
+        colours: {
+            calculator: null,
+            button: null,
+            font: null
+        }
+    };
+
     input.interface.onclick = function () {
         const btn = iframe.document.querySelector('section');
-        if (this.checked == true) btn.classList.add('hide');
-        else btn.classList.remove('hide');
+        btn.classList.toggle('hide');
+        if (this.checked == true) storage.interface.screenOnly = true;
+        else storage.interface.screenOnly = false;
     }
 
     input.colours.calculator.forEach(el => {
-        const color = '#' + el.value;
         el.onclick = () => {
+            const color = '#' + el.value;
+            storage.colours.calculator = color;
             ui.calculator.style.backgroundColor = color;
             ui.buttons.forEach(el => el.style.border = '5px solid ' + color);
         }
     });
     input.colours.button.forEach(el => {
-        const color = '#' + el.value;
-        el.onclick = () => ui.buttons.forEach(el => el.style.backgroundColor = color);
+        el.onclick = () => {
+            const color = '#' + el.value;
+            storage.colours.button = color;
+            ui.buttons.forEach(el => el.style.backgroundColor = color);
+        }
     });
     input.colours.font.forEach(el => {
-        const color = '#' + el.value;
-        el.onclick = () => ui.buttons.forEach(el => el.style.color = color);
+        el.onclick = () => {
+            const color = '#' + el.value;
+            storage.colours.font = color;
+            ui.buttons.forEach(el => el.style.color = color);
+        }
     });
+
+    s('button[type=submit]').onclick = () => {
+        browser.storage.sync.set(storage);
+    }
 })();
 
 //Kadang error harus muat ulang
+//id addon
+//24a6f891de8ef6d442e57b24ac3e899deb7d772a@temporary-addon
+//atau yg di manifest gk pake / diakhir
--- popup/script.js
+++ popup/script.js
@@ -1,11 +1,18 @@
-function $(el) {return document.querySelector(el);}
-function $s(node) {return document.querySelectorAll(node);}
+function $(el) {
+    return document.querySelector(el);
+}
+
+function $s(node) {
+    return document.querySelectorAll(node);
+}
 
 //Click animation
 $s('[class*=ktk-]').forEach(function (el) {
     el.addEventListener('click', function () {
         el.style.opacity = '0';
-        setTimeout(function () {el.style.opacity = '1';}, 100);
+        setTimeout(function () {
+            el.style.opacity = '1';
+        }, 100);
     });
 });
 //End of click animation
@@ -84,20 +91,39 @@ $('.ktk-ce').addEventListener('click', function () {
 });
 
 //Keyboard event
-function keyprees(target){
+function keyprees(target) {
     let x = event.key;
     target.forEach(function (el) {
         if (x == el.innerText) el.click();
         else if (x == el.getAttribute('id')) el.click();
-        else if(x == 'x') window.close();
+        else if (x == 'x') window.close();
     });
 }
 window.addEventListener('keydown', function (e) {
     e.preventDefault();
-    
+
     const dll = [$('.ktk-sma-dgn'), $('.ktk-backspace'), $('.ktk-c'), $('.ktk-ce')];
     keyprees(operand);
     keyprees(operator);
     keyprees(dll);
 });
 //End of keyboard event
+
+browser.storage.sync.get(['colours', 'interface']).then(res => {
+    const ui = {
+        calculator: $('div.container'),
+        buttons: $s('[class*="ktk-"]'),
+    }
+
+    if(res.interface.screenOnly == true) $('section').classList.add('hide');
+
+    ui.calculator.style.backgroundColor = res.colours.calculator;
+    ui.buttons.forEach(el => {
+        el.style.border = '5px solid ' + res.colours.calculator;
+        el.style.backgroundColor = res.colours.button;
+        el.style.color = res.colours.font
+    });
+});
+
+//lanjutin option yg kepilih sma yg keklik ui
+//sma bug yg gak sesuai sma option
